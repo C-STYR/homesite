31870b8b50f38b9e2cfb630d277a3cbc8ef27006
C-STYR <colestyron@gmail.com>
2021-03-01 10:32:12 -0900

    additional components, pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
-const index = () => (
-  <div>
-    <h1>Welcome to my web page</h1>
-    <h2>Hello, there.</h2>
-    <h2>I bet you're wondering why you're here.</h2>
-    <h2>Indeed, how did you get here?</h2>
-  </div>
+import Head from 'next/head'
+import Meta from '../components/Meta'
+import Layout from '../components/Layout'
+import Link from 'next/link'
+
+const Home = () => (
+ 
+    <div>
+      <Meta title="Index"/>
+      <h1>Welcome to my web page</h1>
+      <h2>Hello, there.</h2>
+      <h2>I bet you're wondering why you're here.</h2>
+      <h2>Indeed, how did you get here?</h2>
+    </div>
 )
 
-export default index
+export default Home
---
+++ pages/test.tsx
@@ -0,0 +1,12 @@
+import Meta from '../components/Meta'
+import Layout from '../components/Layout'
+
+const test = () => (
+  
+  <div>
+    <Meta/>
+    <h1>Test</h1>
+  </div>
+)
+
+export default test

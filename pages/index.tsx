27ef74a1ad53463fcf7dd0a2616c87b1de457e76
C-STYR <colestyron@gmail.com>
2021-03-03 12:39:00 -0900

    add python gist

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,12 @@ const Home = () => (
       <Meta title="Home"/>
       <h1>Hello, there.</h1>
       <p>
-        This is the personal website of Cole Styron. It isn't much to look at, I'll grant you, but perhaps 
+        This is the personal website of <span style={{color: "#BDE666"}}> Cole Styron</span>. It isn't much to look at, I'll grant you, but perhaps 
         have a poke around. You might find one or two things of interest if you <i>really look.</i>
       </p>
       <br />
       <p>
-        As a software engineer, I spend a lot of time communicating with the machines. This pastime is not 
+        As a <span style={{color: "#FF555D"}}>software engineer</span>, I spend a lot of time communicating with the <span style={{color: "#F6E183"}}>machines</span>. This pastime is not 
         without interest. Perhaps you'd like to give it a try?
       </p>
     </div>
--- pages/python.tsx
+++ pages/python.tsx
@@ -1,4 +1,5 @@
 import Meta from '../components/Meta'
+import Gist from 'react-gists'
 
 const python = () => {
 
@@ -6,12 +7,26 @@ const python = () => {
   <div>
     <Meta title="Python"/>
     <h1>Python</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-      exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
-      dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla 
-      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
-      deserunt mollit anim id est laborum.</p>
+    <p>
+      Python is one of the most widely used programming languages around today.
+      It is normally found lurking server-side, but frameworks exist for front-end application, too.
+      Python really shines when it comes to data science.
+    </p>
+    <p>
+      I like to use Python for any numerical task I can't calculate in my head (there are no shortage of these).
+    </p>
+    <p>
+      For example, I sometimes like to build loudspeakers. If I want to calculate the port length 
+      (the port is the "hole" on the front or back of the speaker which is filled with a long tube), 
+      I just need a few figures from the design specification.
+    </p>
+    <p>
+    Plugging these into a formula will give me the precise length I need to achieve proper tuning. Below is a
+    Pythonic implementation of this formula.  Simply plug and play. 
+    </p>
+    <h2>Speaker Port Length Calculator</h2>
+    <Gist id="f47af87c04da31253bf2f39b14966495"/>
+    <script src="https://gist.github.com/C-STYR/f47af87c04da31253bf2f39b14966495.js"></script>
   </div>
   )
 }

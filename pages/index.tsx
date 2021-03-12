11ec98b748e7e6c92811a1c7c61089ead24f144b
C-STYR <colestyron@gmail.com>
2021-03-11 22:21:32 -0900

    index boxes final

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,18 @@ import style from '../styles/index.module.scss'
 
 const colors: string[] = ["#D18DF0", " #F6E183", " #BDE666", " #FF555D", " #77A7FF"]
 
-const plage: string[] = ["S", "O", "U", "S", " ", "L", "E", "S", " ", "P", "A", "V", "É", "S"]
+const paves: string[] = ["S", "O", "U", "S", " ", "L", "E", "S", " ", "P", "A", "V", "É", "S"]
+const plage: string[] = ["L", "A", " ", "P", "L", "A", "G", "E"]
 
-const key = (): number => Math.floor(Math.random()*10000)
+const key = (): number => Math.random();
 const colorKey = (): number => Math.floor(Math.random()*5)
 
-const sous: JSX.Element[] = plage.map(e => {
-  if (e === " ") return <div className={style.D} style={{border: "1px solid #001629"}} key={key()}>{e}</div>;
+const sous: JSX.Element[] = paves.map(e => {
+  if (e === " ") return <div className={style.empty} style={{border: "1px solid #001629"}} key={key()}>{e}</div>;
+  else return <div className={style.D} key={key()} style={{border: `1px solid ${colors[colorKey()]}`}}>{e}</div>
+})
+const la: JSX.Element[] = plage.map(e => {
+  if (e === " ") return <div className={style.empty} style={{border: "1px solid #001629"}} key={key()}>{e}</div>;
   else return <div className={style.D} key={key()} style={{border: `1px solid ${colors[colorKey()]}`}}>{e}</div>
 })
 
@@ -27,7 +32,7 @@ const Home = () => (
 
       <p>
         As a <span style={{color: "#FF555D"}}>software engineer</span>, I spend a lot of time communicating 
-        with the <span style={{color: "#F6E183"}}>machines</span>. This pastime is not 
+        with the <span style={{color: "#F6E183"}}>machines</span>. This vocation is not 
         without amusement. Perhaps you'd like to give it a try? You'll find a challenge on 
         the <span className={style.jlink}><Link href='/javascript'>JavaScript</Link></span> page.
       </p>
@@ -37,8 +42,11 @@ const Home = () => (
         <span style={{color: "#77A7FF"}} > Sarah Drasner</span>'s <span className={style.nlink}> 
         <Link href="https://marketplace.visualstudio.com/items?itemName=sdras.night-owl">Night Owl</Link></span>.
       </p>
-      <div className={style.sousContainer}>
+      <div className={style.sousContainer} style={{marginTop: "100px"}}>
         {sous}
+      </div>
+      <div className={style.sousContainer}>
+        {la}
       </div>
     </div>
 
